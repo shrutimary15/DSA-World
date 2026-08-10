9a6870419239711410ea55cd832f8e8cfbffd3bb
Shruti Mary Mathew <77826778+shrutimary15@users.noreply.github.com>
2026-08-10 09:40:05 +0100

    Time: 6 ms (73.88%) | Memory: 19.4 MB (58.51%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -3,13 +3,15 @@ class Solution:
         answer = []
         distinct1 = []
         distinct2 = []
+        nums1 =set(nums1)
+        nums2 = set(nums2)
         for i in nums1:
             if i not in nums2:
                 distinct1.append(i)
         for i in nums2:
             if i not in nums1:
                 distinct2.append(i)
-        answer.append(list(set(distinct1)))
-        answer.append(list(set(distinct2)))
+        answer.append(distinct1)
+        answer.append(distinct2)
         return answer
         
